dd1e3f2415fa38fd3f767d096dec0415ca570e30
Shaiv Sud <shaiv.sud@tmotions.com>
2025-12-23 16:47:50 +0530

    added sidebar

--- frontend.py
+++ frontend.py
@@ -1,17 +1,54 @@
 import streamlit as st
 from backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
 
-if "message_history" not in st.session_state:
+#utility fuctions
+def genrate_thread_id():
+    thread_id=uuid.uuid4()  
+    return thread_id  #random thread
+
+def reset_chat():
+    thread_id= genrate_thread_id()
+    st.session_state['thread_id']=thread_id
+    add_thread(st.session_state['thread_id'])# if reset we need to add the thread
     st.session_state["message_history"] = []
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+#session setup
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = [] # history
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id']=genrate_thread_id() # genrate new thtread for new chat
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads']=[] #adding thread to the side bar
+
+add_thread(st.session_state['thread_id']) #calling the add function
+
+#side bar ui
+st.sidebar.title("langGraph Chatbot")
+
+if st.sidebar.button("New Chat"):
+    reset_chat()
+
+st.sidebar.header("My conversations")
+
+for thread_id in st.session_state['chat_threads']:
+    st.sidebar.button(str(thread_id))
+
 
 # Render history
 for message in st.session_state["message_history"]:
     with st.chat_message(message["role"]):
         st.text(message["content"])
 
-thread_id = "1"
-CONFIG = {"configurable": {"thread_id": thread_id}}
+
+CONFIG = {"configurable": {"thread_id":st.session_state['thread_id']}}
 
 user_input = st.chat_input("type here")
 
